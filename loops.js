cdb42b350060d7c2c6150e4bf71954103ea78595
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-12 16:06:32 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop (arr){
+ 
+ for (var i=0; i<25; i++)
+ {
+   if(i<=1){
+     arr.push( "I am " + i + " strange loop.");}
+   else{
+   
+     arr.push( "I am " + i + " strange loops.");}
+   
+ }
+ 
+return arr;
+}
+
+function whileLoop (n){
+  while(n>=0)
+  { 
+   console.log(n--);
+      
+  }
+  console.log('done');
+}
